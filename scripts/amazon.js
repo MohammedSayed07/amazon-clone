1e9932c72d0fe6bdc331cd43839a904b64c9e1dc
MohammedSayed07 <mohammed.s.hegazi@gmail.com>
2023-06-14 23:57:23 +0300

    Homepage is complete now!

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
   </div>
 
   <div class="product-quantity-container">
-    <select>
+    <select id="${product.id}/selected">
       <option selected value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
@@ -41,16 +41,53 @@ products.forEach((product) => {
 
   <div class="product-spacer"></div>
 
-  <div class="added-to-cart">
+  <div class="added-to-cart" 
+  id="${product.id}/product">
     <img src="images/icons/checkmark.png">
     Added
   </div>
 
-  <button class="add-to-cart-button button-primary">
+  <button class="add-to-cart-button button-primary"
+  id="${product.id}"
+  data-product-id="${product.id}">
     Add to Cart
   </button>
 </div>
 `
 document.querySelector('.products-grid').innerHTML = HTML
+
 });
-  
+
+document.querySelectorAll('.add-to-cart-button').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = String(button.dataset.productId);
+    let matchingItem;
+    let quantity = 0;
+    const element = button.getAttribute('id')
+    
+    let quantityAdded = Number(document.getElementById(element + '/selected').value)
+
+    cart.forEach((item)=> {
+      if (item.productId === productId) {
+        matchingItem = item;
+      }
+    })
+    if(matchingItem) {
+      matchingItem.quantity += quantityAdded;
+    } else {
+      cart.push({
+      productId: productId,
+      quantity: quantityAdded
+    })
+    }
+
+    cart.forEach((item) => {
+      quantity += item.quantity
+    })
+
+    document.querySelector('.cart-quantity').innerText = quantity
+
+    
+    document.getElementById(element + '/product').classList.add('added-to-cart-css')
+  })
+})
